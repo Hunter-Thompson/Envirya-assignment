bc2c1c4aebd1224b1ef56ed639ab576ca81e1c04
aatman <aatman>
2020-08-24 18:58:42 +0530

    change script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source ami_id \
-&& cd Terraform \
+cd Packer \
+&& AMI_ID=$(jq -r '.builds[-1].artifact_id' manifest.json | cut -d ":" -f2) \
+&& cd ../Terraform \
 && yes yes | terraform destroy -var personal_ami=$AMI_ID \
-&& rm -rf .terraform terraform.* \
+
